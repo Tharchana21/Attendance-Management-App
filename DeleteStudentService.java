936627110f63fa51216bb0c8b0206913d428a694
Tharchana S <tharchanasivakumar55@gmail.com>
2024-10-30 00:37:26 +0530

    Create DeleteStudentService.java

---
+++ DeleteStudentService.java
@@ -0,0 +1,10 @@
+package com.besant.services;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface DeleteStudentService {
+
+	public void deleteStudentRecord(HttpServletRequest req, HttpServletResponse res);
+
+}
