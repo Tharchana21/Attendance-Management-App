13070a3748e5fe62d722ac4ea4b1a49a1b06d3ae
Tharchana S <tharchanasivakumar55@gmail.com>
2024-10-30 00:39:34 +0530

    Create UpdateStudentService.java

---
+++ UpdateStudentService.java
@@ -0,0 +1,8 @@
+package com.besant.services;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface UpdateStudentService {
+public void updat(HttpServletRequest req,HttpServletResponse res);
+}
